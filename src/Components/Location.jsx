e7d88206e6fcfeff2a81c3845314f86861687245
msabdalaal <medosayed239@gmail.com>
2022-11-26 03:24:28 +0200

    .

--- src/Components/Location.jsx
+++ src/Components/Location.jsx
@@ -4,29 +4,30 @@ import { fetchLocation } from "../web/API";
 
 export let locationAdress;
 export default function Location({ ip }) {
+  let location = fetchLocation(ip);
   return (
     <div className="flex justify-center mt-8">
       <div className="rounded-md w-full mx-7 lg:w-auto text-center lg:text-left p-8 z-10 min-w-[10px] bg-white flex items-center shadow-lg flex-col md:flex-row lg:flex-row">
         <div className="lg:border-r-[1px] lg:pr-8">
           <p className="text-gray-500 font-medium weight">IP ADDRESS</p>
-          <h2 className="font-semibold text-xl">{fetchLocation(ip).ip}</h2>
+          <h2 className="font-semibold text-xl">{location.ip}</h2>
         </div>
         <div className="lg:pl-8 lg:border-r-[1px] lg:pr-8">
           <p className="text-gray-500 font-medium weight">LOCATION</p>
           <h2 id="Adress" className="font-semibold text-xl">
-            {fetchLocation(ip).city}, {fetchLocation(ip).country}
+            {location.city}, {location.country}
           </h2>
         </div>
         <div className="lg:pl-8 lg:border-r-[1px] lg:pr-8">
           <p className="text-gray-500 font-medium weight">TIMEZONE</p>
           <h2 className="font-semibold text-xl">
-            {fetchLocation(ip).timezone.utc}
+            {location.timezone.utc}
           </h2>
         </div>
         <div className="lg:pl-8">
           <p className="text-gray-500 font-medium weight">ISP</p>
           <h2 className="font-semibold text-xl">
-            {fetchLocation(ip).connection.isp}
+            {location.connection.isp}
           </h2>
         </div>
       </div>
